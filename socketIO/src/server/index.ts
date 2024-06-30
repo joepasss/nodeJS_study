456cb02af4ed6925e358e6c88e6a1ad2636c76e0
joepasss <farwes1180@gmail.com>
2024-06-30 09:12:40 +0900

    socket io server init

--- socketIO/src/server/index.ts
+++ socketIO/src/server/index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import "dotenv/config";
+
+const port = process.env.PORT;
+
+const server = createServer();
+
+const io = new Server(server);
+
+io.on("connection", (socket: Socket) => {
+  console.log(`a user connected!: ${socket.id}`);
+});
+
+server.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
+});
