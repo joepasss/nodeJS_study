79fac44a0a60f74e30f209ef5037dc9e23c440bd
joepasss <farwes1180@gmail.com>
2024-07-02 00:41:52 +0900

    connect socketio server

--- socketIO/src/server/index.ts
+++ socketIO/src/server/index.ts
@@ -1,12 +1,20 @@
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+
 import "dotenv/config";
 
 const port = process.env.PORT;
 
 const server = createServer();
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:9000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket: Socket) => {
   console.log(`a user connected!: ${socket.id}`);
