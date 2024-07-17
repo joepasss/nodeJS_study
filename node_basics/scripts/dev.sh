55c329be9f9baf964251795dec3da9f1040a7c50
joepasss@gentoo <joepasss@gmail.com>
2024-07-18 03:31:01 +0900

    node_basics

--- node_basics/scripts/dev.sh
+++ node_basics/scripts/dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export NODE_OPTIONS="--import ./register-hooks.js"
+export NODE_OPTIONS="--import ./register-hooks.js --no-deprecation"
 npx nodemon --watch "src/**/*.ts" --exec "ts-node" ./src/index.ts
--- node_basics/src/index.ts
+++ node_basics/src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello");
+// CORE MODULES
+// http   : Launch a server, send requests
+// https  : Launch a SSL server
+// fs     : file system
+// path   : path resolve
+// os     : operating system
+
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log(req.method);
+  console.log(req.headers.host);
+
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>MY DOCUMENT</title></head>");
+    res.write("<body><h1>Hello</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (req.url?.startsWith("/api")) {
+    res.setHeader("Content-Type", "Application/json");
+    res.write('{"name": "joe"}');
+    return res.end();
+  }
+});
+
+server.listen(3000);
