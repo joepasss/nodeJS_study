4b8ea6026b813033dd51d5151cd12aff96dd259e
joepasss@gentoo <joepasss@gmail.com>
2024-07-21 04:14:04 +0900

    express.js basics

--- express_basics/src/index.ts
+++ express_basics/src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello");
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
+import adminRouter from "./routes/admin";
+import shopRouter from "./routes/shop";
+import { rootDir, viewsDir } from "./utils/path";
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(rootDir, "public")));
+
+// routes
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+// 404 error
+app.use((_req, res, _next) => {
+  res.status(404).sendFile(path.resolve(viewsDir, "not-found.html"));
+});
+
+app.listen(3000);
---
+++ express_basics/src/utils/path.ts
@@ -0,0 +1,4 @@
+import path from "path";
+
+export const rootDir = process.cwd();
+export const viewsDir = path.join(rootDir, "views");
