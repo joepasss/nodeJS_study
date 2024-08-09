88728a15ab48e588c98c754cd37f0399c56d67e1
joepasss@gentoo <joepasss@gmail.com>
2024-08-10 02:51:17 +0900

    add product route

--- express_basics/server/src/routes/shop.ts
+++ express_basics/server/src/routes/shop.ts
@@ -15,4 +15,24 @@ shopRouter.get("/products", (_req, res, _next) => {
     });
 });
 
+shopRouter.get("/products/:id", (req, res, _next) => {
+  const id = req.params.id;
+
+  getProducts()
+    .then((products) => {
+      const product = products.find((prod) => prod.id === req.params.id);
+
+      if (product === undefined) {
+        logger(`failed to fetch product ${id}`, "ERROR");
+        return res.json({ error: { message: `cannot find product!` } });
+      }
+
+      res.json({ error: undefined, data: product });
+    })
+    .catch((err) => {
+      logger(`failed to fetch products ${err}`, "ERROR");
+      res.status(500).json({ error: { message: "failed to fetch products" } });
+    });
+});
+
 export default shopRouter;
