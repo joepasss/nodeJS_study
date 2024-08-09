70a1cda0e5fae9f980e016db66697b2d53c5a20b
joepasss@gentoo <joepasss@gmail.com>
2024-08-10 02:51:23 +0900

    add cors

--- express_basics/server/src/index.ts
+++ express_basics/server/src/index.ts
@@ -1,15 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import cors, { CorsOptions } from "cors";
 
 import adminRouter from "./routes/admin";
 import shopRouter from "./routes/shop";
 import { rootDir } from "./utils/path";
-import { logger } from "./controllers/error";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const corsOptions: CorsOptions = {
+  origin: "http://172.30.1.86:9000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 // routes
 app.use("/uploads", express.static(path.join(rootDir, "uploads")));
@@ -18,8 +26,6 @@ app.use("/api", shopRouter);
 
 // 404 error
 app.use("/", (_req, res, _next) => {
-  logger("route not found", "ERROR");
-
   res.status(404).json({
     error: {
       message: "route not found!",
